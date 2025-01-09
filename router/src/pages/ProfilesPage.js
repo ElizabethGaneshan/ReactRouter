87dd500f68168cd0823508860d1783929da1d677
Elizabethg <Elizabethg@gnrgy.com>
2025-01-09 11:58:30 +0530

    Created few profiles based on profile id

--- router/src/pages/ProfilesPage.js
+++ router/src/pages/ProfilesPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const profiles = [1, 2, 3, 4, 5];
 
 const ProfilesPage = () => {
   return (
-    <div>ProfilesPage</div>
-  )
-}
+    <div className="flex flex-col gap-2">
+      {profiles?.map((profile, index) => {
+        return (
+          <Link key={index} to={`/profiles${profile}`}>
+            Profile {profile}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
-export default ProfilesPage
+export default ProfilesPage;
