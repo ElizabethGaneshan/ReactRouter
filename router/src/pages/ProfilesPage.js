603a3db10212b10308459fc93dce25f604132228
Elizabethg <Elizabethg@gnrgy.com>
2025-01-09 15:38:39 +0530

    Replaced the Link tag with the NavLink

--- router/src/pages/ProfilesPage.js
+++ router/src/pages/ProfilesPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const profiles = [1, 2, 3, 4, 5];
 
@@ -9,9 +9,15 @@ const ProfilesPage = () => {
       <div className="flex flex-col gap-2">
         {profiles?.map((profile, index) => {
           return (
-            <Link key={index} to={`/profiles/${profile}`}>
+            <NavLink
+              key={index}
+              to={`/profiles/${profile}`}
+              className={({isActive}) => {
+                return isActive ? "text-danger-700" : "";
+              }}
+            >
               Profile {profile}
-            </Link>
+            </NavLink>
           );
         })}
       </div>
