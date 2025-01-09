48a0584eb2907163f60ea924e36773faabd7094d
Elizabethg <Elizabethg@gnrgy.com>
2025-01-09 11:52:24 +0530

    Added the path, element and errorElement props

--- router/src/index.js
+++ router/src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage";
+import ProfilesPage from "./pages/ProfilesPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <div>404 Not Found</div>,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/profiles",
+    element: <ProfilesPage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ router/src/pages/NotFoundPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <div className="d-flex  align-items-center justify-content-center vh-100">
+        404 Not Found
+        {/* Link tag from react(does not  refresh the whole page) */}
+        <Link to={"/"}>Home from Link</Link>;
+        {/* native anchor tag (refreshes the whole page) */}
+        <a href="/">Home from A</a>
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
