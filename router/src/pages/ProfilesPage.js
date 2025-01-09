8ddfbbb0131ae8d53a5af9a69aba033a89c64acc
Elizabethg <Elizabethg@gnrgy.com>
2025-01-09 12:17:46 +0530

    Rendering the child component using <Outlet /> in the parent component itself

--- router/src/pages/ProfilesPage.js
+++ router/src/pages/ProfilesPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const profiles = [1, 2, 3, 4, 5];
 
@@ -13,6 +13,7 @@ const ProfilesPage = () => {
           </Link>
         );
       })}
+      <Outlet />
     </div>
   );
 };
